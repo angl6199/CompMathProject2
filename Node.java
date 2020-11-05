811b9d28e61c08ad463eaec500805ab028a757db
Angel Heredia <angl.vaz6199@gmail.com>
2020-11-05 14:45:31 -0600

    Create Node class

---
+++ Node.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Node {
+
+    private String data = null;
+
+    private List<Node> children = new ArrayList<>();
+
+    private Node parent = null;
+
+    public Node(String data) {
+        this.data = data;
+    }
+
+    public void addChild(Node child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
+
+
+    public List<Node> getChildren() {
+        return children;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    private void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    @Override
+    public String toString() {
+        return data;
+    }
+
+
+}
