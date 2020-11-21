bc60bbaca0c49b7183b0bcc0b20784c692772d03
Conde <a01650549@itesm.mx>
2020-11-21 15:12:31 -0600

    Progress in javadocs
    Enter queue method missing

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+  // These are the variables where the data of the file will be stored
   private static String[] nonterminals;
   private static String[] terminals;
   private static String initial;
@@ -21,9 +22,18 @@ public class Main {
     int maxdepth = sc.nextInt();
     System.out.println();
 
-    BuildTree(input, maxdepth);
+    Node root = BuildTree(input, maxdepth);
+    generateSyntax(root);
   }
 
+  /**
+   * 
+   * This method reads the data from the file line by line and saves all the
+   * components in its corresponding variables.
+   *
+   * @param name the name of the file including .txt
+   *
+   */
   public static void ReadFile(String name) throws FileNotFoundException, IOException {
     // These are the objects that will allow us to read the data from the txt file
     File file = new File(name);
@@ -36,7 +46,7 @@ public class Main {
     while ((line = br.readLine()) != null) {
 
       if (counter == 0) {
-        // This refers to line 0 which has all the states of the DFA
+        // This refers to line 0 which has all the nonterminal symbols
         nonterminals = SplitWithComma(line);
         for (String string : nonterminals) {
           System.out.println(string);
@@ -44,7 +54,7 @@ public class Main {
       }
 
       if (counter == 1) {
-        // This refers to line 1 which has all the characters of the alphabet of the DFA
+        // This refers to line 1 which has all the terminal symbols
         terminals = SplitWithComma(line);
         for (String string : terminals) {
           System.out.println(string);
@@ -52,13 +62,13 @@ public class Main {
       }
 
       if (counter == 2) {
-        // This refers to line 2 which has the initial state of the DFA
+        // This refers to line 2 which has the initial symbol
         initial = line;
         System.out.println(initial);
       }
 
       if (counter >= 3) {
-        // This refers to lines 4 and further which have all the transitions of the DFA
+        // This refers to lines which have the productions
         String[] array = SplitProductions(line);
         if (table.isEmpty()) {
           table.put(1, array);
@@ -74,24 +84,46 @@ public class Main {
     PrintTable2();
   }
 
+  /**
+   * 
+   * Returns an array of strings to be saved. Used for reading the file.
+   * 
+   * * This method separates the received line from the file into multiple strings
+   * whenever it finds a comma and stores them in an array
+   *
+   * @param line a string containing data from the file to be separated with
+   *             commas.
+   *
+   * @return the data stored in an array.
+   * 
+   */
   public static String[] SplitWithComma(String line) {
     return line.split(",");
   }
 
+  /**
+   * Returns an array of strings containing the productions.
+   * 
+   * This method is used when reading the data from the file. It separates the
+   * received line from the file into multiple strings whenever it finds an arrow,
+   * then it stores them in an array.
+   *
+   * @param line a string containing a production with the following form: S->a
+   * 
+   * @return production of the automata
+   * 
+   */
   public static String[] SplitProductions(String line) {
     String[] array = line.split("->");
     return array;
   }
 
-  public static void PrintTable() {
-    Enumeration enumeration = table.elements();
-    Enumeration llaves = table.keys();
-    while (enumeration.hasMoreElements()) {
-      System.out.println("Llave: " + llaves.nextElement() + " Valor: " + enumeration.nextElement());
-
-    }
-  }
-
+  /**
+   * This method is used after storing the data from the file. It gets the table
+   * that contains the productions and prints it. First, it prints the number of
+   * production (key of the table) and then its content in the format A->a
+   *
+   */
   public static void PrintTable2() {
     for (int i = 0; i < table.size(); i++) {
       String temp = i + 1 + " " + table.get(i + 1)[0] + "->" + table.get(i + 1)[1];
@@ -99,6 +131,17 @@ public class Main {
     }
   }
 
+  /**
+   * Returns an int that is the depth of the given node of the tree.
+   * 
+   * This method is used after inserting a node in the tree. It finds the depth of
+   * the given node.
+   *
+   * @param temp a node recently inserted in the tree
+   * 
+   * @return depth of the node
+   * 
+   */
   public static int calculateDepth(Node temp) {
     int counter = 0;
     while (temp.getParent() != null) {
@@ -108,27 +151,33 @@ public class Main {
     return counter;
   }
 
-  public static void printTree(Node node, String appender) {
-    System.out.println(appender + node.getData());
-    node.getChildren().forEach(each -> printTree(each, appender + appender));
-  }
-
-  public static void BuildTree(String input, int maxdepth) {
+  /**
+   * Returns a node that is the root of the tree.
+   * 
+   * This method is used after asking the user the string to find and the maximum
+   * depth. It makes a top-down parsing and stops when maximum depth is exceeded
+   * or the string given by the user is found. It prints if the string was
+   * accepted or not.
+   *
+   * @param input    string that the user wants to process
+   * @param maxdepth int that is the maximum depth wanted
+   * 
+   * @return root of the created tree
+   * 
+   */
+  public static Node BuildTree(String input, int maxdepth) {
     Node root = new Node(initial);
     Queue<Node> Q = new LinkedList<>();
     Q.add(root);
     String uwv = "";
     int actualdepth = 0;
 
-    // While verde
     while (!Q.isEmpty() && !input.equals(uwv) && actualdepth <= maxdepth) {
-
       Node q = Q.poll();
       int i = 0;
       boolean done = false;
       String varsust = GetLeft(q);
 
-      // While rojo
       while (!done && !input.equals(uwv)) {
         if (!MoreRules(varsust, i)) {
           done = true;
@@ -141,10 +190,7 @@ public class Main {
 
           if (EnterQueue(uwv, input)) {
             Q.add(temp);
-          } else { // else prueba
-            System.out.println(uwv + " was not inserted");
           }
-
           i = j;
           actualdepth = calculateDepth(temp);
           if (actualdepth > maxdepth) {
@@ -153,7 +199,7 @@ public class Main {
         }
       }
     }
-    printTree(root, " ");
+
     System.out.println();
 
     if (input.equals(uwv) && actualdepth <= maxdepth) {
@@ -163,10 +209,25 @@ public class Main {
       System.out.println("Input: " + input + " was not accepted.");
     }
     System.out.println();
-    generateSyntax(root);
+
+    return root;
 
   }
 
+  /**
+   * Returns a string with the leftmost nonterminal character.
+   * 
+   * This method is used to find the head of the production of a given string. It
+   * compares each character of the string with each of the nonterminal symbols
+   * until it finds the first one. Null will be returned if nonterminal symbols
+   * are not found.
+   *
+   * @param q node that contains a string.
+   * 
+   * 
+   * @return leftmost nonterminal character.
+   * 
+   */
   public static String GetLeft(Node q) {
     for (int j = 0; j < q.getData().length(); j++) {
       for (int k = 0; k < nonterminals.length; k++) {
@@ -178,6 +239,18 @@ public class Main {
     return null;
   }
 
+  /**
+   * Returns a boolean stating if there are more rules with head varsust.
+   * 
+   * This method is used inside the buildTree method to determine whether there
+   * are more rules with the given head.
+   *
+   * @param varsust string corresponds to the head of a production
+   * @param i       int that corresponds to the last rule found
+   * 
+   * @return boolean that states whether another rule was found o not
+   * 
+   */
   public static boolean MoreRules(String varsust, int i) {
     for (int j = i + 1; j <= table.size(); j++) {
       if (table.get(j)[0].equals(varsust)) {
@@ -187,6 +260,19 @@ public class Main {
     return false;
   }
 
+  /**
+   * Returns an int that corresponds to the number of the production with head
+   * varsust.
+   * 
+   * This method is used inside the buildTree method to determine the number of
+   * production required.
+   *
+   * @param varsust string corresponds to the head of a production
+   * @param i       int that corresponds to the last rule found
+   * 
+   * @return int that contains the number of the rule with head varsust
+   * 
+   */
   public static int FindProduction(String varsust, int i) {
     for (int j = i + 1; j <= table.size(); j++) {
       if (table.get(j)[0].equals(varsust)) {
@@ -196,25 +282,27 @@ public class Main {
     return -1;
   }
 
-  public static boolean FindinAlphabet(String uwv) {
-    for (int i = 0; i < uwv.length(); i++) {
-      for (int j = 0; j < nonterminals.length; j++) {
-        if (uwv.charAt(i) == nonterminals[j].charAt(0)) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
+  /**
+   * Returns a boolean that corresponds whether a node should be added to the
+   * queue.
+   * 
+   * This method is used inside the buildTree method to determine if the given
+   * string fulfills the characteristics to be inserted in the queue. If uwv does
+   * not belong to Σ* and the terminal prefix matches a prefix in the input.
+   * 
+   *
+   * @param uwv   string corresponds to the head of a production
+   * @param input int that corresponds to the last rule found
+   * 
+   * @return int that contains the number of the rule with head varsust
+   * 
+   */
   public static boolean EnterQueue(String uwv, String input) {
-
     for (int i = 0; i < nonterminals.length; i++) {
       if (uwv.charAt(0) == nonterminals[i].charAt(0)) {
         return true;
       }
     }
-
     String uwvtrimmed = "";
     int index = 0;
     boolean found = false;
@@ -224,59 +312,44 @@ public class Main {
         if (uwv.charAt(i) == nonterminals[j].charAt(0) && found == false) {
           index = i;
           found = true;
-
         }
       }
     }
-
     uwvtrimmed = uwv.substring(0, index);
 
     if (uwvtrimmed.length() > input.length()) {
       return false;
     } else {
-
       for (int i = 0; i < uwvtrimmed.length(); i++) {
         if (uwvtrimmed.charAt(i) != input.charAt(i)) {
           return false;
         }
       }
       return true;
-
     }
-
   }
 
   public static void generateSyntax(Node root) {
     String result = "";
-    int counter = 0;
-    Stack<Node> queue = new Stack<>();
-    queue.add(root);
+    Stack<Node> stack = new Stack<>();
+    stack.add(root);
 
-    while (!queue.isEmpty()) {
-      Node nodeTemp = queue.pop();
+    while (!stack.isEmpty()) {
+      Node nodeTemp = stack.pop();
       result += "[" + nodeTemp.getData();
 
-      // Si no tiene hijos su remaining es cero
       if (nodeTemp.getChildren().isEmpty()) {
         nodeTemp.setRemaining(0);
 
-        // Si no tiene papá, poner ]
         if (nodeTemp.getParent() == null) {
           result += "]";
         } else {
-          // Si tiene papá
-          // si su remaining es cero, poner ]
           if (nodeTemp.getRemaining() == 0) {
             result += "]";
-
           }
-          // ir al papá, restarle uno
-
           Node papa = nodeTemp.getParent();
           papa.setRemaining(papa.getRemaining() - 1);
 
-          // Si el papá es cero poner v1 counter++ v2 poner ], v3 subir hasta que haya
-          // padres, cada cero agrega un ]
           while (nodeTemp.getParent() != null && nodeTemp.getParent().getRemaining() == 0) {
             nodeTemp = nodeTemp.getParent();
             if (nodeTemp.getRemaining() == 0 && !nodeTemp.getDeuda()) {
@@ -284,59 +357,24 @@ public class Main {
               nodeTemp.setDeuda(true);
             }
           }
-
         }
-
       } else {
-        // si tiene hijos
         if (!nodeTemp.getChildren().isEmpty()) {
-          // Si tiene papá
           if (nodeTemp.getParent() != null) {
-            // ir al papá y restarle uno, actualizar su remaining =
-            // #hijos
-
             Node papa = nodeTemp.getParent();
             papa.setRemaining(papa.getRemaining() - 1);
             List<Node> hijos = nodeTemp.getChildren();
             nodeTemp.setRemaining(hijos.size());
-
-            // Si el papá se hace cero v1 counter++ v3 no va
-            if (papa.getRemaining() == 0) {
-              counter++;
-            }
-
-            // Meter hijos a la cola
-            queue.addAll(hijos);
-
+            stack.addAll(hijos);
           } else {
-            // Si no tiene papá, meto los hijos a la cola y le pongo su remaining = # hijos
             List<Node> hijos = nodeTemp.getChildren();
             nodeTemp.setRemaining(hijos.size());
-            queue.addAll(hijos);
+            stack.addAll(hijos);
           }
-
         }
-
       }
     }
-    // Por cada uno del counter agregar ]
-    for (int i = 0; i < counter; i++) {
-      // result += "]";
-
-    }
-
     System.out.println(result);
-
-  }
-
-  public static int countOcurrences(String s, char t) {
-    int result = 0;
-    for (int i = 0; i < s.length(); i++) {
-      if (s.charAt(i) == t) {
-        result++;
-      }
-    }
-    return result;
   }
 
 }
